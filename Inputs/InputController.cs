a888356a42f94e5f80a2ae074ce3de1162339c15
dgboy <dan.gorbunov.00@yandex.ru>
2022-05-31 22:28:21 +0300

    add: inputs

---
+++ Inputs/InputController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour {
+    [SerializeField] private Notification inputCheck = null;
+    [SerializeField] private Notification inputInventory = null;
+    [SerializeField] private Notification inputCancel = null;
+
+    [SerializeField] private PlayerMovement player = null;
+    public GenericAbility[] abilities;
+    private int current = 0;
+
+    public void SwitchSkill(InputAction.CallbackContext context) {
+        if (!context.started) {
+            return;
+        }
+        current = (current + 1 < abilities.Length) ? current + 1 : 0;
+        // player.Ability = abilities[current];
+    }
+    // private PlayerInput _input;
+    // private bool dialogMap = false;
+
+
+    public void OnMove(InputAction.CallbackContext context) {
+        player.TempMovement = context.ReadValue<Vector2>();
+        // Debug.Log(player.TempMovement);
+    }
+    public void OnClickMove(InputAction.CallbackContext context) {
+        Vector2 movement = context.ReadValue<Vector2>();
+        //player.TempMovement = context.ReadValue<Vector2>();
+
+        //Movement.Normalize();
+        //player.rigidbody.MovePosition(player.position + movement * 0.1f);
+    }
+
+    public void OnAttack(InputAction.CallbackContext context) {
+        player.Attacking();
+    }
+    public void OnAbility(InputAction.CallbackContext context) {
+        // player.UseAbility();
+    }
+    public void OnCheck(InputAction.CallbackContext context) {
+        if (!context.started) {
+            return;
+        }
+        // Debug.Log("Check!");
+        if (!player.IsReceiveItem) {
+            inputCheck.Raise();
+            // ChangeActiveMap("Dialog");
+        } else {
+        }
+    }
+    public void OnContinue(InputAction.CallbackContext context) {
+        if (!context.started) {
+            return;
+        }
+        inputCheck.Raise();
+    }
+    public void OnInventory(InputAction.CallbackContext context) {
+        inputInventory.Raise();
+        // ChangeActiveMap("Inventory");
+    }
+    public void OnPause(InputAction.CallbackContext context) {
+        inputCancel.Raise();
+    }
+
+    // public void SwitchToPlayerMap() {
+    //     DisableDialog();
+    //     EnablePlayer();
+    // }
+    // public void SwitchToDialogMap() {
+    //         DisablePlayer();
+    //     // if (!dialogMap) {
+    //     //     EnableDialog();
+    //     //     dialogMap = true;
+    //     // } else {
+    //     //     SwitchToPlayerMap();
+    //     //     dialogMap = false;
+    //     // }
+    // }
+    // public void SwitchToInventoryMap() {
+    //     DisablePlayer();
+    //     EnableInventory();
+    // }
+
+    // public void Awake() {
+    //     _input = new PlayerInput();
+    //     // PlayerInput.SwitchCurrentActionMap("Player");
+    //     // EnablePlayer();
+    // }
+
+    // void OnEnable() => _input.Enable();
+    // void OnDisable() => _input.Disable();
+
+    // void Update() {
+    //     // player.TempMovement = _input.Player.Move.ReadValue<Vector2>();
+    //     // player.Motion(direction);
+    //     // if (direction.magnitude > 0) {
+    //     //     player.Walking(direction);
+    //     // } else {
+    //     //     player.Idling();
+    //     // }
+    // }
+
+
+    // void EnablePlayer() {
+    //     Debug.Log(_input.Player);
+    //     _input.Player.Check.performed += context => OnCheck(context);
+    //     _input.Player.Attack.performed += context => OnAttack(context);
+    //     _input.Player.Ability.performed += context => OnAbility(context);
+    // }
+    // void DisablePlayer() {
+    //     _input.Player.Check.performed -= context => OnCheck(context);
+    //     _input.Player.Attack.performed -= context => OnAttack(context);
+    //     _input.Player.Ability.performed -= context => OnAbility(context);
+    // }
+
+    // void EnableDialog() {
+    //     _input.Dialog.Continue.performed += context => OnContinue(context);
+    // }
+    // void DisableDialog() {
+    //     _input.Dialog.Continue.performed -= context => OnContinue(context);
+    // }
+
+    // void EnableInventory() {
+    //     _input.UI.Submit.performed += context => OnContinue(context);
+    // }
+    // void DisableInventory() {
+    //     _input.UI.Submit.performed -= context => OnContinue(context);
+    // }
+}
---
+++ Inputs/PlayerMouse.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class PlayerMouse : MonoBehaviour
+{
+    public static bool blockInput = false;
+    public float speed = 3.0f;
+    //private new Rigidbody2D rigidbody;
+    //private Vector3 Movement;
+
+    public Tilemap map;
+    public PlayerMovement player;
+    private PlayerInput playerInput;
+    private Vector3 destination;
+
+    private void Awake() {
+        playerInput = new PlayerInput();
+    }
+    public void OnEnable() {
+        playerInput.Enable();
+    }
+    public void OnDisable() {
+        playerInput.Disable();
+    }
+
+    public void Start() {
+        destination = transform.position;
+        playerInput.Player.LeftClick.performed += _ => MouseClick();
+    }
+
+    public IEnumerator Check() {
+        yield return new WaitForSeconds(3f);
+        Debug.Log(player.TempMovement);
+    }
+
+    public void MouseClick() {
+        if (blockInput) return; // move global
+
+        Vector2 mousePosition = playerInput.Player.MousePosition.ReadValue<Vector2>();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector3Int gridPosition = map.WorldToCell(mousePosition);
+        if (map.HasTile(gridPosition)) {
+            destination = mousePosition;
+            var magnitude = -(transform.position - destination).magnitude;
+            player.TempMovement = (transform.position - destination) / magnitude * speed;
+        }
+    }
+
+    public void LateUpdate() {
+        if (Vector3.Distance(transform.position, destination) > 0.1f) {
+            //transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            //player.rigidbody.velocity = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+            //player.rigidbody.AddForce(Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime));
+            //player.Motion(player.TempMovement);
+        } else {
+            player.TempMovement = Vector2.zero;
+        }
+    }
+}
