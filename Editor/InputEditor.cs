19e3594755eda249b7e3fa4adddbce94e2280514
dg-maker <dan.gorbunov.00@yandex.ru>
2023-04-23 00:37:43 +0300

    feat: basic auto app builder [CI]

--- Editor/InputEditor.cs
+++ Editor/InputEditor.cs
@@ -1,22 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using UnityEditor;
 
-//namespace UtilityPack {
-//    [CustomEditor(typeof(UIMoveable), true)]
-//    public class InputEditor : Editor {
-//        public override void OnInspectorGUI() {
-//            //var script = target as UIMoveable;
+namespace DG_Pack.Editor {}
 
-//            //script.useThis = GUILayout.Toggle(script.useThis, "Use This Transform");
+namespace UtilityPack {
+    // [CustomEditor(typeof(UIMoveable), true)]
+    public class InputEditor : Editor {
+        public override void OnInspectorGUI() {
+            //var script = target as UIMoveable;
 
-//            //if (!script.useThis) {
-//            //    script.Target = EditorGUILayout.PropertyField(serializedObject.FindProperty(script._target, true);
-//            //    SerializedProperty target = serializedObject.FindProperty("Target");
-//            //    //script.Target = EditorGUILayout.PropertyField(target, new GUIContent("Target"));
-//            //}
+            //script.useThis = GUILayout.Toggle(script.useThis, "Use This Transform");
 
-//        }
-//    }
-//}
+            //if (!script.useThis) {
+            //    script.Target = EditorGUILayout.PropertyField(serializedObject.FindProperty(script._target, true);
+            //    SerializedProperty target = serializedObject.FindProperty("Target");
+            //    //script.Target = EditorGUILayout.PropertyField(target, new GUIContent("Target"));
+            //}
+
+        }
+    }
+}
