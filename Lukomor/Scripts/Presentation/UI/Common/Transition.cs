1b9fbde1a18bb637cb9614578fa740c052ce5e6e
dg-maker <dan.gorbunov.00@yandex.ru>
2023-04-23 01:13:45 +0300

    del: Lukomor

--- Lukomor/Scripts/Presentation/UI/Common/Transition.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Threading.Tasks;
-using UnityEngine;
-
-namespace Lukomor.Presentation.Common {
-	public abstract class Transition : MonoBehaviour {
-		public abstract Task Play();
-	}
-}
