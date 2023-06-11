d0bf61a0fea20efac8684b296508621152a266b0
dg-maker <dan.gorbunov.00@yandex.ru>
2023-06-12 01:53:46 +0300

    fix: bad behavior of the health bar

--- UI/Bars/Points/PointBar.cs
+++ UI/Bars/Points/PointBar.cs
@@ -4,37 +4,29 @@ using UnityEngine.UI;
 
 public class PointBar : MonoBehaviour {
     [SerializeField] private Image heart;
-    [SerializeField] private int unitFactor = 2;
     [SerializeField] private Sprite[] states;
-    public Sprite fullHeart;
-    public Sprite halfHeart;
-    public Sprite emptyHeart;
-    
+
     private readonly List<Image> _items = new List<Image>();
+    private int StateFactor => states.Length - 1;
 
 
     public void Init(int max, int value) {
         _items.AddRange(GetComponentsInChildren<Image>());
         // transform.Clear();
         Refresh(max, value);
+        // Debug.Log($"[{gameObject.name}] health: {value} / {max}");
     }
-    
-    public void Refresh(int max, float value) {
-        int maxCount = max / 2;
-        Debug.Log($"health: {value} / {max}");
 
-        if (maxCount > _items.Count) Create();
+    public void Refresh(int max, int value) {
+        if (max / StateFactor > _items.Count) Create();
+        else Clear(max);
 
-        for (int i = 0; i < maxCount; i++) {
+        Fill(value);
+    }
+    private void Fill(int value) {
+        for (int i = 0; i < _items.Count; i++) {
             var image = _items[i].GetComponent<Image>();
-
-            if (i + 1 <= value) {
-                image.sprite = fullHeart;
-            } else if (i >= value) {
-                image.sprite = emptyHeart;
-            } else {
-                image.sprite = halfHeart;
-            }
+            image.sprite = states[Mathf.Clamp(value - i * StateFactor, 0, StateFactor)];
         }
     }
 
@@ -43,5 +35,12 @@ public class PointBar : MonoBehaviour {
         var newPoint = Instantiate(heart, transform);
         newPoint.name = $"point {transform.childCount}";
         _items.Add(newPoint);
+    }
+    
+    private void Clear(int max) {
+        for (int i = _items.Count; i > 0; i--) {
+            if (i > max / StateFactor)
+                Destroy(_items[i]);
+        }
     }
 }
