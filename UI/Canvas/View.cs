31b659915cab3958390f13f8205392d5e40201db
dg-lord <dan.gorbunov.00@yandex.ru>
2024-05-19 23:38:40 +0300

    UI Canvas: View

---
+++ UI/Canvas/View.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DG_Pack.UI.Canvas {
+    public abstract class View : MonoBehaviour {
+        protected List<IHandler> Handlers { get; set; }
+
+
+        private void Awake() {
+            Init();
+            Handlers.ForEach(x => x.Bind(this));
+        }
+        private void Start() => Handlers.ForEach(x => x.Refresh());
+        private void OnDestroy() => Handlers.ForEach(x => x.Unbind());
+
+
+        protected abstract void Init();
+    }
+}
