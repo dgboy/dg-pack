7f5b3f437af8382b8f5ec95caa8636e7982eeaca
dg-lord <dan.gorbunov.00@yandex.ru>
2024-05-22 23:27:55 +0300

    del: useless TabMenu

--- UI/TabMenu/Base/ITab.cs
+++
@@ -1,7 +0,0 @@
-namespace DG_Pack.UI.TabMenu.Base {
-    public interface ITab {
-        bool Active { get; set; }
-        void SetGroup(ITabMenu value);
-        void Select(bool on, bool sendEvent = false);
-    }
-}
--- UI/TabMenu/Base/ITabMenu.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace DG_Pack.UI.TabMenu.Base {
-    public interface ITabMenu {
-        event Action<int, bool> OnSelect;
-        int Index { get; set; }
-        void Select(ITab tab, bool doAction = true);
-    }
-}
