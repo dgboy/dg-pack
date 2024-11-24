13e88f93f660b50907ecc9a6946632be0cc87739
dg-lord <dan.gorbunov.00@yandex.ru>
2024-11-24 19:20:00 +0300

    Prototype Stuff

---
+++ Prototype/Stuff/Magic.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Core.Player.Generic_Components {
+    public class Magic : MonoBehaviour {
+        [SerializeField] private float maxMagic;
+        private int currentMagic;
+
+        public int Max { get => (int)maxMagic; set => maxMagic = (int)value; }
+        public int Current { get => currentMagic; }
+        public bool IsExhausted => (currentMagic <= 0);
+
+
+        public bool CanUseMagic(int amount) => currentMagic >= amount;
+
+        public void FillMagic() => currentMagic = Max;
+        public void SetHealth(int amount) => currentMagic = amount;
+        public void UseAllMagic() => currentMagic = 0;
+
+
+        public virtual void UseMagic(int amount) {
+            currentMagic = (currentMagic - amount <= 0) ? 0 : currentMagic - amount;
+        }
+        public void RestoreMagic(int amount) {
+            currentMagic = (currentMagic + amount > Max) ? Max : currentMagic + amount;
+        }
+
+        void Start() {
+            FillMagic();
+        }
+    }
+}
