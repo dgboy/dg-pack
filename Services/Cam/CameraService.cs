a311581b0b62b31497fe1393485f26be05c99086
dg-lord <dan.gorbunov.00@yandex.ru>
2024-12-03 21:31:13 +0300

    add: CameraService

---
+++ Services/Cam/CameraService.cs
@@ -0,0 +1,12 @@
+﻿using DG_Pack.Base;
+using UnityEngine;
+
+namespace DG_Pack.Services.Cam {
+    public static class CameraService {
+        private static Camera Main => Camera.main;
+
+
+        public static Vector3 ToWorldPoint(Vector3 mousePosition) =>
+            Main!.ScreenToWorldPoint(mousePosition.To3(-Main.transform.position.z));
+    }
+}
