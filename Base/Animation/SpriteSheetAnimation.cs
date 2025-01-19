3e1ee75f3d47511700858599550b2d6e2314324e
dg-lord <dan.gorbunov.00@yandex.ru>
2025-01-19 18:58:36 +0300

    добавить простую анимацию набора спрайтов

---
+++ Base/Animation/SpriteSheetAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DG_Pack.Base.Animation {
+    public class SpriteSheetAnimation : MonoBehaviour {
+        public SpriteRenderer sr;
+        public float fps;
+
+        public List<Sprite> sheet;
+
+
+        private void Update() {
+            sr.sprite = sheet[(int)(Time.time * fps) % sheet.Count];
+        }
+    }
+}
