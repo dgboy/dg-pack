9787b5ff677f56bf6ca28c6cb6b9c2ed0efcba71
dg-lord <dan.gorbunov.00@yandex.ru>
2025-01-24 16:24:22 +0300

    присваивать дефолтные значения на валидации в инспекторе

--- Prototype/SpriteSheetAnimation.cs
+++ Prototype/SpriteSheetAnimation.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 namespace DG_Pack.Prototype {
     public class SpriteSheetAnimation : MonoBehaviour {
         public SpriteRenderer sr;
-        public float fps;
 
+        public float fps = 6;
         public List<Sprite> sheet;
 
 
+        private void OnValidate() {
+            sr = GetComponent<SpriteRenderer>();
+
+            if (sheet.Count > 0)
+                sr.sprite = sheet[0];
+        }
         private void Update() {
             sr.sprite = sheet[(int)(Time.time * fps) % sheet.Count];
         }
