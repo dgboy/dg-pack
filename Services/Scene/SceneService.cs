4d6c6890c0c94558b9ff5f10cab5dffe313725a4
dg-lord <dan.gorbunov.00@yandex.ru>
2025-01-28 00:07:42 +0300

    убрать из конструктора сервиса загрузки сцен кастомный логер

--- Services/Scene/SceneService.cs
+++ Services/Scene/SceneService.cs
@@ -4,17 +4,13 @@ using UnityEngine.SceneManagement;
 
 namespace DG_Pack.Services.Scene {
     public class SceneService : ISceneService {
-        public SceneService(ICustomLogger logger) => _logger = logger;
-
-        private readonly ICustomLogger _logger;
         public string Current => SceneManager.GetActiveScene().name;
 
 
         public async Task Load(string scene) {
             if (Current == scene) return;
 
-            _logger.LogTransition(this, Current, scene);
-
+            DLogger.LogTransition(this, Current, scene);
             var operation = SceneManager.LoadSceneAsync(scene);
 
             while (operation is { isDone: false })
