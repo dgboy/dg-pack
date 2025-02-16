40b7e54bd9deb49fb46a6e1a07c0530de944ab0f
dg-lord <dan.gorbunov.00@yandex.ru>
2025-02-16 22:47:27 +0300

    добавил возможность удобно работать с Direction (Vector2 <-> Direction4)

--- Prototype/Movement.cs
+++ Prototype/Movement.cs
@@ -5,7 +5,8 @@ namespace DG_Pack.Prototype {
         public Direction4 direction;
         public Vector2 delta;
         public bool active;
-
+        
+        public Vector2 Direction { get => direction.ToVector2(); set => direction = value.From(); }
 
         public void Apply(Vector2 value) {
             delta = value;
