be9c5a87861ff4227fdc22a6e0552c3b37c92cf3
Daniil Gorbunov <dan.gorbunov.00@yandex.ru>
2025-02-27 23:13:25 +0300

    исправил неостанавливаемый ход в MoveToCell

--- Prototype/MoveToCell.cs
+++ Prototype/MoveToCell.cs
@@ -1,28 +1,36 @@
-﻿using UnityEngine;
+﻿using DG_Pack.Services.Log;
+using UnityEngine;
+using UnityEngine;
 
 namespace DG_Pack.Prototype {
     public class MoveToCell : MonoBehaviour {
         public float speed = 5.0f;
         public float cellSize = 1.0f;
         private bool _isMoving;
-        private Vector3 _direction;
+        private Vector2 _direction;
         private Vector2Int _target;
 
         private void Update() {
-            if (_isMoving) {
-                float step = speed * Time.deltaTime;
-                transform.position = Vector2.MoveTowards(transform.position, _target, step);
+            Vector2 position = transform.position;
 
-                if (Vector2.Distance(transform.position, _target) < 0f)
+            if (_isMoving) {
+                var towards = Vector2.MoveTowards(position, _target, speed * Time.deltaTime);
+
+                // if (position == towards)
+                //     DLogger.Log($"{transform.name} stuck!", this);
+
+                transform.position = towards;
+
+                if (Vector2.Distance(position, _target) == 0f)
                     _isMoving = false;
             } else {
                 _direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
                 // _direction = new Vector3(_direction.x > 0f ? 1f : 0, _direction.y > 0f ? 1f : 0);
-                    
-                if (_direction == Vector3.zero)
+
+                if (_direction == Vector2.zero)
                     return;
 
-                var pos = transform.position + _direction * cellSize;
+                var pos = position + _direction * cellSize;
                 _target = new Vector2Int(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.y));
                 _isMoving = true;
             }
